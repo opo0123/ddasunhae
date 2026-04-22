8b49ab470aa9de164928795ac2c270c69145609d
opo0123 <dldkdud05@gmail.com>
2026-04-22 18:01:11 +0900

    코드펜 추가 및 sponsor js 수정 및 반응형 적용 (sponsor 완료)

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # favicon, header~cat story html 끝, css 작업중
+
 # figma-변수 생성 / 반응형 작업, inner 등 다 수정
+
 # 반응형 모두 수정(clamp), 후원 이야기 - sponsor html 작업
+
 # sponsor pc 작업, 내일 코드펜 추가하고 시작하기
+
+# 코드펜 추가 및 sponsor js 수정 및 반응형 적용 (sponsor 완료)
--- js/sponsor.js
+++ js/sponsor.js
@@ -1,36 +1,109 @@
-// jQuery.fn.liScroll = function(settings) {
-// 	settings = jQuery.extend({
-// 		travelocity: 0.03
-// 		}, settings);		
-// 		return this.each(function(){
-// 				var $strip = jQuery(this);
-// 				$strip.addClass("newsticker")
-// 				var stripHeight = 1;
-// 				$strip.find("li").each(function(i){
-// 					stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
-// 				});
-// 				var $mask = $strip.wrap("<div class='mask'></div>");
-// 				var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
-// 				var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width 	
-// 				$strip.height(stripHeight);			
-// 				var totalTravel = stripHeight;
-// 				var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
-// 				function scrollnews(spazio, tempo){
-// 				$strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", containerHeight); scrollnews(totalTravel, defTiming);});
-// 				}
-// 				scrollnews(totalTravel, defTiming);				
-// 				$strip.hover(function(){
-// 				  jQuery(this).stop();
-// 				},
-// 				function(){
-// 				  var offset = jQuery(this).offset();
-// 				  var residualSpace = offset.top + stripHeight;
-// 				  var residualTime = residualSpace/settings.travelocity;
-// 				  scrollnews(residualSpace, residualTime);
-// 				});			
-// 		});	
-// };
+jQuery.fn.liScroll = function (settings) {
+  settings = jQuery.extend(
+    {
+      travelocity: 0.03,
+    },
+    settings,
+  );
 
-// $(function(){
-//     $("ul#ticker01").liScroll();
-// });
+  return this.each(function () {
+    var $strip = jQuery(this);
+    $strip.addClass("newsticker");
+
+    // 리스트 복제 (끊김 없는 반복)
+    $strip.append($strip.html());
+
+    var stripHeight = 0;
+    $strip.find("li").each(function () {
+      stripHeight += jQuery(this).outerHeight(true);
+    });
+
+    $strip.wrap("<div class='mask'></div>");
+    $strip.parent().wrap("<div class='tickercontainer'></div>");
+
+    var pos = 0;
+    var rafId = null;
+
+    function scrollnews() {
+      pos -= 0.5;
+
+      // 절반 지나면 초기화 (무한 루프)
+      if (Math.abs(pos) >= stripHeight / 2) {
+        pos = 0;
+      }
+
+      $strip.css("top", pos);
+
+      rafId = requestAnimationFrame(scrollnews);
+    }
+
+    scrollnews();
+  });
+};
+
+$(function () {
+  $("ul#ticker01").liScroll({
+    travelocity: 0.05,
+  });
+
+
+
+  // tablet, mobile
+  jQuery.fn.liScroll = function (settings) {
+    settings = jQuery.extend(
+      {
+        travelocity: 0.03,
+      },
+      settings,
+    );
+
+    return this.each(function () {
+      var $strip = jQuery(this);
+      $strip.addClass("newsticker");
+
+      // 가로 배치 추가
+      $strip.css({
+        display: "flex",
+        position: "relative",
+        left: 0,
+      });
+
+      // 리스트 복제 (끊김 없는 반복)
+      $strip.append($strip.html());
+
+      var stripWidth = 0;
+      $strip.find("li").each(function () {
+        stripWidth += jQuery(this).outerWidth(true);
+      });
+
+      $strip.width(stripWidth);
+
+      $strip.wrap("<div class='mask2'></div>");
+      $strip.parent().wrap("<div class='tickercontainer2'></div>");
+
+      var pos = 0;
+      var rafId = null;
+
+      function scrollnews() {
+        pos -= 0.5;
+
+        // 절반 지나면 초기화 (무한 루프)
+        if (Math.abs(pos) >= stripWidth / 2) {
+          pos = 0;
+        }
+
+        $strip.css("left", pos);
+
+        rafId = requestAnimationFrame(scrollnews);
+      }
+
+      scrollnews();
+    });
+  };
+
+  $(function () {
+    $(".ticker02 ul").liScroll({
+      travelocity: 0.05,
+    });
+  });
+});
