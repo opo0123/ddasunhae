81b1e29d4863c90559fabcc6118222952902c117
opo0123 <dldkdud05@gmail.com>
2026-04-28 18:08:19 +0900

    모달 css 작업중

---
+++ js/modal_gotop.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", function () {
+  // 모달창 닫기
+  const modalWrap = this.document.querySelector(".modal-wrap");
+  const modalClose = this.document.querySelector(".modalClose");
+  modalClose.addEventListener("click", () => {
+    modalWrap.style.display = "none"
+  });
+  
+  // gotop 스크롤 이벤트
+  // 변수 선언
+  const gotop = this.document.querySelector("#gotop");
+  this.window.addEventListener("scroll", () => {
+    if (this.document.documentElement.scrollTop > 1000) {
+      gotop.style.display = "block";
+    } else {
+      gotop.style.display = "none";
+    }
+  });
+
+  // gotop 버튼 클릭시 위로 스크롤 (부드럽게)
+  gotop.addEventListener("click", () => {
+    this.window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
